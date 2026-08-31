2539fa4a239cbf73839f6edac08dc049a3b3645b
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-08-31 19:38:24 +0200

    Update 3_Nuovo_Preventivo.py

--- pages/3_Nuovo_Preventivo.py
+++ pages/3_Nuovo_Preventivo.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Nuovo Preventivo", page_icon="💰")
+
+st.title("💰 Nuovo Preventivo")
+st.write("Qui creeremo un preventivo partendo da un progetto esistente.")
