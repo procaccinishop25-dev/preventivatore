1930103d168706556f8d83270d62da63abf59514
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-09-05 13:16:15 +0200

    Update __init__.py

--- components/drawing_editor/__init__.py
+++ components/drawing_editor/__init__.py
@@ -7,13 +7,11 @@ _component_func = components.declare_component(
 )
 
 
-def drawing_editor(title=None, background_image_url=None, initial_state=None, width=1100, height=700, key=None):
+def drawing_editor(title=None, background_image_url=None, initial_state=None, key=None):
     return _component_func(
         title=title,
         background_image_url=background_image_url,
         initial_state=initial_state,
-        width=width,
-        height=height,
         key=key,
         default=None,
     )
